f571a48e83293c019c1071b74f58fc71e6cd0f40
nophramel <damir.ebibi@gmail.com>
2023-06-01 11:15:28 +0200

    added first backend codesnippets

--- client/script.js
+++ client/script.js
@@ -7,6 +7,11 @@ socket.addEventListener("open", (event) => {
 
 socket.addEventListener("message", (event) => {
   console.log(`Received message: ${event.data}`);
+  // TODO: Füge den empfangenen Nachrichteninhalt zum Chatfenster hinzu
+  const chatMessages = document.querySelector(".chat-messages");
+  const newMessage = document.createElement("li");
+  newMessage.textContent = event.data;
+  chatMessages.appendChild(newMessage);
 });
 
 socket.addEventListener("close", (event) => {
@@ -15,4 +20,14 @@ socket.addEventListener("close", (event) => {
 
 socket.addEventListener("error", (event) => {
   console.error("WebSocket error:", event);
+});
+
+// TODO: Event-Handler für das Texteingabefeld und den Senden-Button
+const textarea = document.querySelector("textarea");
+const sendButton = document.querySelector("button");
+
+sendButton.addEventListener("click", () => {
+  const message = textarea.value;
+  socket.send(message);
+  textarea.value = ""; // Leere das Texteingabefeld nach dem Senden
 });
